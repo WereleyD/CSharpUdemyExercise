9360bf72a96ff434698b533a93236a455afbc89c
Wesley <wesley@coopsantaclara.com.br>
2023-01-25 12:43:17 -0300

    first commit

---
+++ CSharpUdemy/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpUdemy
+{
+    public class BankAccount
+    {
+        public string AccountName { get; private set; }
+        public int AccountNumber { get; private set; } = 0;
+        public double AccountBalance { get; private set; }
+        public double WithdrawFee { get; private set; } = 5;
+
+        public void setAccountName(string name)
+        {
+            AccountName = name;
+        }
+        public void setAccountNumber(int accountNumber) { 
+            if(AccountNumber == 0)
+            {
+                AccountNumber = accountNumber;
+            }
+            else
+            {
+                Console.WriteLine("It's not possible to change the account number.");
+            }
+        }
+
+        public void Withdraw(double withdrawValue)
+        {
+            if(withdrawValue > 0)
+            {
+                AccountBalance -= withdrawValue + WithdrawFee;
+            }
+        }
+        public void Deposit(double depositValue)
+        {
+            if (depositValue > 0)
+            {
+                AccountBalance += depositValue;
+            }
+            else { Console.WriteLine("Invalid value. Try again."); }
+        }
+        public void InitialDeposit(char op)
+        {
+            if (op == 'y')
+            {
+                Console.Write("What's the value of your initial deposit? ");
+                double depositValue = double.Parse(Console.ReadLine());
+                Deposit(depositValue);
+            }
+            else if (op == 'n')
+            {
+                Deposit(0);
+            }
+            else
+            {
+                Console.WriteLine("Invalid option. Try again.");
+            }
+        }
+        public void AccountInformation()
+        {
+            Console.WriteLine("Account Information:\n" +
+                $"Account Number: {AccountNumber}, Account Owner: {AccountName}, Balance: $ {AccountBalance}");
+        }
+    }
+}
---
+++ CSharpUdemy/Program.cs
@@ -0,0 +1,26 @@
+﻿using CSharpUdemy;
+
+BankAccount firstAccount = new BankAccount();
+
+Console.Write("Enter de account number: ");
+int accountNumber = int.Parse(Console.ReadLine());
+firstAccount.setAccountNumber(accountNumber);
+Console.Write("Enter the name of the account's ownner: ");
+string accountOwnerName = Console.ReadLine();
+firstAccount.setAccountName(accountOwnerName);
+Console.WriteLine("There will be an initial deposit? Y/N");
+char initialDeposit = char.Parse(Console.ReadLine());
+firstAccount.InitialDeposit(initialDeposit);
+Console.WriteLine("");
+firstAccount.AccountInformation();
+Console.WriteLine("");
+Console.Write("Enter a deposit value: ");
+double depositValue = double.Parse(Console.ReadLine());
+firstAccount.Deposit(depositValue);
+firstAccount.AccountInformation();
+Console.WriteLine("");
+Console.Write("Enter a withdraw value: ");
+double withdrawValue = double.Parse(Console.ReadLine());
+firstAccount.Withdraw(withdrawValue);
+firstAccount.AccountInformation();
+
