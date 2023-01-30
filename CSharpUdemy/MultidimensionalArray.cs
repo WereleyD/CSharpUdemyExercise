953a05cd00a56d129d076806100f432f75d04034
Wesley <wesley@coopsantaclara.com.br>
2023-01-30 17:07:49 -0300

    Bidimensional Array Exercise

---
+++ CSharpUdemy/MultidimensionalArray.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpUdemy
+{
+    public class MultidimensionalArray
+    {
+
+        public static void MainDiagonal(int x, int[,] matriz)
+        {
+            Console.Write("Main diagonal: ");
+            int diagonal = 0;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    if (j == diagonal)
+                    {
+                        Console.Write(matriz[i, j] + " ");
+                    }
+                }
+                diagonal++;
+            }
+        }
+
+        public static void NegativeNumbers(int x, int[,] matriz)
+        {
+            int negative = 0;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    if (matriz[i, j] < 0)
+                    {
+                        negative++;
+                    }
+                }
+            }
+            Console.Write($"Negative Numbers = {negative}");
+        }
+
+        public static void FindNumbers(int x, int y, int[,] matriz)
+        {
+            int op = int.Parse(Console.ReadLine());
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (matriz[i, j] == op)
+                    {
+                        //left right up down
+                        /* 3 4 
+                         * 10 8 15 12
+                         * 21 11 23 8
+                         * 14 5 13 19
+                         */
+                        Console.WriteLine($"Position {i}, {j}");
+                        if (j - 1 >= 0)
+                        {
+                            Console.WriteLine($"Left: {matriz[i, j - 1]}");
+                        }
+                        if (j + 1 <= x)
+                        {
+                            Console.WriteLine($"Right: {matriz[i, j + 1]}");
+                        }
+                        if(i - 1 >= 0)
+                        {
+                            Console.WriteLine($"Up: {matriz[i - 1, j]}");
+                        }
+                        if(i + 1 <= x)
+                        {
+                            Console.WriteLine($"Down: {matriz[i + 1, j]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- CSharpUdemy/Program.cs
+++ CSharpUdemy/Program.cs
@@ -1,5 +1,83 @@
 ﻿using CSharpUdemy;
 
+int n = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+
+int[,] mat = new int[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    string[] str = Console.ReadLine().Split(' ');
+    for (int j = 0; j < m; j++)
+    {
+        mat[i,j] = int.Parse(str[j]);
+    }
+}
+
+MultidimensionalArray.FindNumbers(n, m, mat);
+
+
+
+//MultidimensionalArray
+
+Console.Write("Size: ");
+int x = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[x, x];
+
+for (int i = 0; i < x; i++)
+{
+    string[] values = Console.ReadLine().Split(' '); //guarda no vetor os valores que foram separados por espaço.
+    for (int j = 0; j < x; j++)
+    {
+        matriz[i, j] = int.Parse(values[j]);
+    }
+}
+
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < x; j++)
+    {
+        Console.Write(matriz[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+MultidimensionalArray.MainDiagonal(x, matriz);
+MultidimensionalArray.NegativeNumbers(x, matriz);
+Console.ReadLine();
+
+
+//RoomsRented
+
+Console.Write("How many rooms will be rented? ");
+int op = int.Parse(Console.ReadLine());
+RoomsRented[] vect = new RoomsRented[10];
+
+for (int i = 0; i < op; i++)
+{
+    Console.WriteLine($"Rent #{i + 1}");
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("E-mail: ");
+    string email = Console.ReadLine();
+    Console.Write("Room: ");
+    int roomNum = int.Parse(Console.ReadLine());
+    vect[roomNum] = new RoomsRented(name, email);
+}
+for (int i = 0; i < 10; i++)
+{
+    if (vect[i] != null)
+    {
+        Console.WriteLine($"Room {i}");
+        Console.WriteLine(vect[i]);
+        Console.WriteLine();
+    }
+}
+Console.ReadLine();
+
+//BankAccount
+
 Console.Write("Enter de account number: ");
 int accountNumber = int.Parse(Console.ReadLine());
 
